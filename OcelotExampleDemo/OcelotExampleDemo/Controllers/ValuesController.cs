f9a55ec0dc4868442e98a9c59142197fcecf4daf
Petar Ilijasic <petar.ilijasic@avl.com>
2022-06-21 09:41:23 +0200

    Ocelot examples
    Added Ocelot examples for .NET Core WEB API

---
+++ OcelotExampleDemo/OcelotExampleDemo/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OcelotExampleDemo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public async Task<string> GetName()
+        {
+            await Task.Delay(6000);
+            return "Jonathan";
+        }
+    }
+}
---
+++ OcelotHTTPSAPI/OcelotHTTPSAPI/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OcelotHTTPSAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public async Task<string> GetName()
+        {
+            await Task.Delay(3600);
+            return "Jonathan";
+        }
+    }
+}
