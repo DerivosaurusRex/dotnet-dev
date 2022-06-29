dc37b5ae772d5fa287412cf4f4b08e904acffe32
Petar Ilijasic <petar.ilijasic@avl.com>
2022-06-29 15:04:48 +0200

    City Controller uses SQLite Repository
    Replaced in-memory data storage with DB storage in City controller

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,6 @@
-﻿using CityInfo.API.Models;
+﻿using CityInfo.API.Entities;
+using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -7,27 +9,58 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController: ControllerBase 
     {
-        private readonly CitiesDataStore _citiesDataStore;
+        //private readonly CitiesDataStore _citiesDataStore;
+        
+        private readonly ICityInfoRepository _cityInfoRepository;
 
-        public CitiesController(CitiesDataStore citiesDataStore)
+        //Works with local in-memory database
+        //public CitiesController(CitiesDataStore citiesDataStore)
+        //{
+        //    _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+        //}
+
+        public CitiesController(ICityInfoRepository cityInfoRepository )
         {
-            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+            _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
         {
-           return Ok(_citiesDataStore.Cities);
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+            var results = new List<CityWithoutPointsOfInterestDto>();
+
+            foreach(var city in cityEntities)
+            {
+                results.Add(new CityWithoutPointsOfInterestDto()
+                {
+                    Id = city.Id,
+                    Name = city.Name,
+                    Description = city.Description
+                });
+            }
+
+            return Ok(results);
+
+           //return Ok(_citiesDataStore.Cities);
+    
         }
 
         [HttpGet("{id}")]
-        public ActionResult<CityDto> GetCity(int id)
+        public async Task<ActionResult<CityWithoutPointsOfInterestDto>> GetCity(int id)
         {
-            var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+            //var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+            
+            var cityToReturn = await _cityInfoRepository.GetCityAsync(id, false);
+            var result = new CityWithoutPointsOfInterestDto()
+            {
+                Id = cityToReturn.Id,
+                Name = cityToReturn.Name,
+                Description = cityToReturn.Description
+            };
 
             if (cityToReturn == null) return NotFound();
-            else return Ok(cityToReturn);
-            //return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+            else return Ok(result);
         }
     }
 }
