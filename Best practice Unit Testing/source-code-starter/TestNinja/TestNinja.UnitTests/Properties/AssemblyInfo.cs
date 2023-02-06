13d3771bdc1ef4a45e521a8310a9c552c1772a6f
pilijasic <petarilijasic@gmail.com>
2023-02-06 08:06:01 +0100

    Added a few unit tests

---
+++ Best practice Unit Testing/source-code-starter/TestNinja/TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AVL List GmbH")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © AVL List GmbH 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37f837ec-935b-4885-8bd9-eb646f9755b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Best practice Unit Testing/source-code-starter/TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+  [TestFixture]
+  public class ReservationTests
+  {
+    [Test]
+    public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+    {
+      //Arange
+      var reservation = new Reservation();
+
+      //Act
+      var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+      //Assert
+      Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnsTrue()
+    {
+      //Arange
+      var reservation = new Reservation();
+      var testUser = new User();
+      reservation.MadeBy = testUser;
+
+      //Act
+      var result = reservation.CanBeCancelledBy(testUser);
+
+      //Assert
+      Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_UserIsNotAdmin_ReturnsFalse()
+    {
+      //Arange
+      var reservation = new Reservation();
+
+      //Act 
+      var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+
+      //Asser
+      Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_AnotherUserCancelling_ReturnFalse()
+    {
+      //Arrange
+      var reservation = new Reservation() { MadeBy = new User() };
+
+      //Act
+      var result = reservation.CanBeCancelledBy(new User());
+
+      //Assert
+      Assert.IsFalse(result);
+    }
+  }
+}
